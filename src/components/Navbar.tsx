40439a63a17e9214f18ea58fbdce1f8b0bb9d7f0
ClaytonDewey <clay@dryadmedia.com>
2023-02-22 16:45:56 -0600

    Setup Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <nav>
+      <h1>sneakers</h1>
+      <i>Menu Item 1</i>
+    </nav>
+  );
+};
+export default Navbar;
