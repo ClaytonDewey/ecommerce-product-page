115d1c3aea959533e501a1463005133a719f4231
ClaytonDewey <clay@dryadmedia.com>
2023-02-24 14:13:14 -0600

    Update Icons

--- src/Icons/next.tsx
+++ src/Icons/next.tsx
@@ -3,13 +3,7 @@ import React from 'react';
 const IconNext: React.FC = () => {
   return (
     <svg width='13' height='18' xmlns='http://www.w3.org/2000/svg'>
-      <path
-        d='m2 1 8 8-8 8'
-        stroke='#1D2026'
-        stroke-width='3'
-        fill='none'
-        fill-rule='evenodd'
-      />
+      <path d='m2 1 8 8-8 8' strokeWidth='3' fill='none' fillRule='evenodd' />
     </svg>
   );
 };
--- src/Icons/previous.tsx
+++ src/Icons/previous.tsx
@@ -3,13 +3,7 @@ import React from 'react';
 const IconPrevious: React.FC = () => {
   return (
     <svg width='12' height='18' xmlns='http://www.w3.org/2000/svg'>
-      <path
-        d='M11 1 3 9l8 8'
-        stroke='#1D2026'
-        stroke-width='3'
-        fill='none'
-        fill-rule='evenodd'
-      />
+      <path d='M11 1 3 9l8 8' strokeWidth='3' fill='none' fillRule='evenodd' />
     </svg>
   );
 };
