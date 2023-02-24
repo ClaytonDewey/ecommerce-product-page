604b1288d1c8ebbd24f34495ce9d3201fb9d17c4
ClaytonDewey <clay@dryadmedia.com>
2023-02-23 22:47:27 -0600

    Add photos for ImageSlider

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export const productImages = [
+  require('./images/image-product-1.jpg'),
+  require('./images/image-product-2.jpg'),
+  require('./images/image-product-3.jpg'),
+  require('./images/image-product-4.jpg'),
+];
