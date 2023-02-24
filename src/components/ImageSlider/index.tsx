98f429495d9d2d6339b9e3a192099e51914aa99d
ClaytonDewey <clay@dryadmedia.com>
2023-02-23 22:49:23 -0600

    Create slider using swiper

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SwiperCore, { Navigation, Thumbs } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { productImages } from '../../assets';
 
 type ImageSliderProps = {};
 
 const ImageSlider: React.FC<ImageSliderProps> = () => {
+  const [activeThumb, setActiveThumb] = useState<SwiperCore>();
+
   return (
-    <img
-      className='img-responsive'
-      src='./images/image-product-1.jpg'
-      alt='a shoe!'
-    />
+    <div className='image-container'>
+      <Swiper
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        modules={[Navigation, Thumbs]}
+        grabCursor={true}
+        thumbs={{
+          swiper: activeThumb && !activeThumb.destroyed ? activeThumb : null,
+        }}
+        className='product-images-slider'
+      >
+        {productImages.map((image, idx) => {
+          return (
+            <SwiperSlide key={idx}>
+              <img className='img-responsive' src={image} alt='product thumb' />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <Swiper
+        onSwiper={setActiveThumb}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        modules={[Navigation, Thumbs]}
+        className='product-images-slider-thumbs'
+      >
+        {productImages.map((image, idx) => {
+          return (
+            <SwiperSlide key={idx}>
+              <div className='product-images-slider-thumbs-wrapper'>
+                <img
+                  className='img-responsive'
+                  src={image}
+                  alt='product thumb'
+                />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 };
 export default ImageSlider;
