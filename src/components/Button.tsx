30cf796c77c495b83553e0bc267ac943f029cb90
ClaytonDewey <clay@dryadmedia.com>
2023-02-25 14:52:10 -0600

    Work on resizing svg icons properly.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,25 +7,32 @@ type ButtonProps = {
 };
 
 const StyledButton = styled.button`
-  background-color: hsl(26, 100%, 55%);
-  border: 0;
+  background-color: var(--brand);
   cursor: pointer;
-  color: #fff;
+  border: 0;
+  color: var(--white);
   height: 5.5rem;
-  width: 25rem;
+  width: 27rem;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 1rem;
   font-weight: 700;
+  transition: opacity 0.25s ease-in-out, box-shadow 0.25s ease-in-out;
+  font-size: 1.6rem;
 
   svg {
     fill: white;
-    stroke: white;
+    margin-right: 1.5rem;
+    /* height: 1.6rem;
+    width: 1.6rem; */
   }
 
-  img {
-    margin-right: 2rem;
+  &:hover {
+    opacity: 0.8;
+    cursor: pointer;
+    transition: opacity 0.25s ease-in-out, box-shadow 0.25s ease-in-out;
+    box-shadow: 0 3rem 2rem hsla(26, 100%, 55%, 0.2);
   }
 `;
 
